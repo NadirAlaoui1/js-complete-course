3f18ddf4347856e7bdfba7c1847b473d4a53eebe
nadiralaoui1 <nadiralaoui23@gmail.com>
2019-09-03 09:33:24 +0200

    addition

--- 02-maths/script.js
+++ 02-maths/script.js
@@ -12,19 +12,11 @@
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
+
     document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+        let result = parseInt(document.getElementById("op-one").value)
+                    + parseInt(document.getElementById("op-two").value);
+        window.alert(result);
     });
 
-    document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
-    });
-
-    document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
-    });
-
-    document.getElementById("division").addEventListener("click", function() {
-        // perform an division
-    });
 })();
